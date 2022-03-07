820630d0a30cc187d1103bbe7eab9e2010e76a32
GeorgeM101 <georgmboya@gmail.com>
2022-03-08 02:34:28 +0300

    added login path

--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def home(request):
@@ -27,3 +27,5 @@ def register(request):
             print('user created')
             return redirect('login')
     return render(request,'register.html')
+
+
