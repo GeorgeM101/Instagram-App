fd2c2dea3cb11aa40c8ffe0c87a22d0593d767d4
GeorgeM101 <georgmboya@gmail.com>
2022-03-09 12:10:38 +0300

    created path for profile

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='home'),
     path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('search/', views.search_results, name='search_results')
