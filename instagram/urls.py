07ab4398176ecdee52542693a945535e4d101bfa
GeorgeM101 <georgmboya@gmail.com>
2022-03-09 12:08:33 +0300

    added profile section

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,8 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name= 'home'),
+    path('', views.index, name='home'),
     path('register/', views.register, name='register'),
-    path('login/',views.login,name='login'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('search/', views.search_results, name='search_results')
 ]
